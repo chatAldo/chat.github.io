12d60a89d216d4afcb6ce1a1f7b14c5ae53c52ef
chatAldo <153471640+chatAldo@users.noreply.github.com>
2023-12-10 21:13:28 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyANEx5_pjc4p4aG1t60FQgv8RBMff72R_k",
+  apiKey: "AIzaSyANEx5_pjc4p4aG1t60FQgv8RBMff72R_k",
   authDomain: "chat-cf36f.firebaseapp.com",
   projectId: "chat-cf36f",
   storageBucket: "chat-cf36f.appspot.com",
