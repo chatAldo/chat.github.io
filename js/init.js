bd76543e9b3e7538a5dd4d2981fa8ef1ad994916
chatAldo <153471640+chatAldo@users.noreply.github.com>
2023-12-10 20:37:22 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,10 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyA8RriQJJtfl4MxbKtSwl1uBxmH48clyF0",
-  authDomain: "chatborrado-89c70.firebaseapp.com",
-  projectId: "chatborrado-89c70",
-  storageBucket: "chatborrado-89c70.appspot.com",
-  messagingSenderId: "1002843686378",
-  appId: "1:1002843686378:web:6d2dfed2f6e207c82a5e93",
-  measurementId: "G-2GNRVBFBK2"
+apiKey: "AIzaSyDqEb3Tx5tgDJrGF0asXDCcXKNRKS1Ww7I",
+  authDomain: "chat43aoau-c816b.firebaseapp.com",
+  projectId: "chat43aoau-c816b",
+  storageBucket: "chat43aoau-c816b.appspot.com",
+  messagingSenderId: "255659018506",
+  appId: "1:255659018506:web:b02c969a1168b193aed2f3"
     });
